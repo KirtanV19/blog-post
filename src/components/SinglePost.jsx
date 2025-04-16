5e7409fa4e0616e0874ac0313248425f243da3b3
IPS-153 <ips-153@IPS-153s-iMac.local>
2025-04-16 14:44:31 +0530

    first commit

---
+++ src/components/SinglePost.jsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from 'react'
+import { useParams } from 'react-router'
+import { getPost } from '../assets/API'
+
+const SinglePost = () => {
+
+    const { id } = useParams()
+    console.log(id);
+
+
+    const user = async () => {
+
+        await getPost(id)
+    }
+
+    useCallback(() => {
+
+    }, [])
+    const memoizedCallback = useCallback(
+        () => {
+            // Your function logic here
+        },
+        [dependencies] // This function will be re-created only when dependencies change
+    );
+
+
+    return (
+        <div>SinglePost</div>
+    )
+}
+
+export default SinglePost
